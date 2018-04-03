04878b0711308dc12f301b6a12663286fb5211c7
JohnGG <jonathan.grandperrin1@gmail.com>
2018-04-03 21:09:05 +0200

    mlk

---
+++ linear_model.py
@@ -0,0 +1,16 @@
+import tensorflow as tf
+
+input = tf.placeholder(shape=(), name="input", dtype=tf.float32)
+
+theta0 = tf.placeholder(shape=(), name="theta0", dtype=tf.float32)
+
+theta1 = tf.placeholder(shape=(), name="theta1", dtype=tf.float32)
+
+mul = theta0 * input
+
+prediction = mul + theta1
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    result = sess.run(prediction, feed_dict={input:1, theta1:5, theta0:-1.2})
+    print(result)
